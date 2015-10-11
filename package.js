46334eef68998125d2b773c297743313ba50c5f2
cannap <sh4fty@gmail.com>
2015-10-11 15:39:07 +0200

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cannap:error-notification',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Shows Browser Notification on any Meteor.Error',
   // URL to the Git repository containing the source code for this package.
