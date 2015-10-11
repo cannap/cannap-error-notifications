df73b58f6b9bf3e54b9e381f553ad8929c1ff472
cannap <sh4fty@gmail.com>
2015-10-11 15:08:53 +0200

    first release

--- error-notification.js
+++ error-notification.js
@@ -1,1 +1,21 @@
-// Write your package code here!
+var original = Meteor._debug;
+
+
+if (window.Notification && !Notify.needsPermission) {
+    Meteor._debug = function (err, stack) {
+
+
+        var myNotification = new Notify(err, {
+            body: stack,
+            icon: '/packages/cannap_error-notification/img/meteor.png'
+        });
+
+        myNotification.show();
+
+        return original;
+    }
+
+
+} else if (Notify.isSupported()) {
+    Notify.requestPermission();
+}
--- package.js
+++ package.js
@@ -7,13 +7,17 @@ Package.describe({
   git: '',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: 'README.md',
+  debugOnly:true
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.0.2');
+
   api.use('ecmascript');
-  api.addFiles('error-notification.js');
+  api.addAssets('img/meteor.png','client');
+  api.addFiles('vendor/notify.js','client');
+  api.addFiles('error-notification.js','client');
 });
 
 Package.onTest(function(api) {
